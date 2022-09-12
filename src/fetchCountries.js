0454b783951f429b4b27e76b4ed5e0b64bc35909
Torianyk Alex <torianyk.alex@gmail.com>
2022-09-12 20:06:49 +0300

    adds projct

---
+++ src/fetchCountries.js
@@ -0,0 +1,14 @@
+const options = 'fields=name,capital,population,flags,languages';
+
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+
+export default function fetchCountries(name) {
+  return fetch(`${BASE_URL}${name}?${options}`)
+    .then(res => {
+      if (res.status === 404) {
+        return Promise.reject(new Error());
+      }
+      return res.json();
+    })
+    .catch(error => console.log(error));
+}
